09d09f6dbb10d8f142808d981d855661a2f9e96a
GlebKosianchuk <83186808+GlebKosianchuk@users.noreply.github.com>
2024-12-13 13:37:29 +0300

    Update

--- Tasks/RockPaperScissorsGame/RockPaperScissorsGame.cs
+++ Tasks/RockPaperScissorsGame/RockPaperScissorsGame.cs
@@ -7,29 +7,98 @@ namespace Tasks.RockPaperScissorsGame
         Scissors
     }
 
-    public class Game
+    public enum GameResult
     {
-        public string Play(Move playerMove, Move pcMove)
-        {
-            if (playerMove == pcMove)
-            {
-                return "Draw!";
-            }
+        Win,
+        Loss,
+        Draw
+    }
 
-            return playerMove switch
-            {
-                Move.Rock => (pcMove == Move.Scissors) ? "You win!" : "You loss!",
-                Move.Paper => (pcMove == Move.Rock) ? "You win!" : "You loss!",
-                Move.Scissors => (pcMove == Move.Paper) ? "You win" : "You loss!",
-                _ => throw new ArgumentOutOfRangeException(nameof(playerMove), "The move should be Rock, Paper, or Scissors.")
-            };
+    public interface IPlayer
+    {
+        Move MakeMove();
+    }
+
+    public class RandomComputerPlayer : IPlayer
+    {
+        private readonly Random _random;
+
+        public RandomComputerPlayer(Random random)
+        {
+            _random = random;
         }
 
-        public Move GetPcMove()
+        public Move MakeMove()
         {
-            var random = new Random();
             var values = Enum.GetValues(typeof(Move));
-            return (Move)values.GetValue(random.Next(values.Length));
+            return (Move)values.GetValue(_random.Next(values.Length));
+        }
+    }
+
+    public class Game
+    {
+        private readonly IPlayer _player1;
+        private readonly IPlayer _player2;
+        private readonly int _bestOfN;
+
+        public Game(IPlayer player1, IPlayer player2, int bestOfN)
+        {
+            _player1 = player1;
+            _player2 = player2;
+            _bestOfN = bestOfN;
+        }
+
+        public GameResult Play()
+        {
+            var player1Wins = 0;
+            var player2Wins = 0;
+
+            while (player1Wins < (_bestOfN / 2) + 1 && player2Wins < (_bestOfN / 2) + 1)
+            {
+                Move player1Move = _player1.MakeMove();
+                Move player2Move = _player2.MakeMove();
+
+                GameResult result = DetermineWinner(player1Move, player2Move);
+
+                if (result == GameResult.Win)
+                {
+                    player1Wins++;
+                }
+                else if (result == GameResult.Loss)
+                {
+                    player2Wins++;
+                }
+            }
+
+            if (player1Wins > player2Wins)
+            {
+                return GameResult.Win;
+            }
+            else if (player2Wins > player1Wins)
+            {
+                return GameResult.Loss;
+            }
+            else
+            {
+                return GameResult.Draw;
+            }
+        }
+
+        public GameResult DetermineWinner(Move player1Move, Move player2Move)
+        {
+            if (player1Move == player2Move)
+            {
+                return GameResult.Draw;
+            }
+
+            return player1Move switch
+            {
+                Move.Rock => player2Move == Move.Scissors ? GameResult.Win : GameResult.Loss,
+                Move.Paper => player2Move == Move.Rock ? GameResult.Win : GameResult.Loss,
+                Move.Scissors => player2Move == Move.Paper ? GameResult.Win : GameResult.Loss,
+                _ => throw new ArgumentOutOfRangeException(nameof(player1Move),
+                    "The move should be Rock, Paper, or Scissors.")
+            };
         }
     }
 }
--- Tests/RockPaperScissorsGameTests/RockPaperScissorsGameTests.cs
+++ Tests/RockPaperScissorsGameTests/RockPaperScissorsGameTests.cs
@@ -1,3 +1,4 @@
+using Moq;
 using Tasks.RockPaperScissorsGame;
 
 namespace Tests.RockPaperScissorsGameTests;
@@ -5,45 +6,54 @@ namespace Tests.RockPaperScissorsGameTests;
 public class RockPaperScissorsGameTests
 {
     [Test]
+        public void DetermineWinner_RockVsScissors_Player1Wins()
+        {
+            var game = new Game(null, null, 1);
+            var result = game.DetermineWinner(Move.Rock, Move.Scissors);
+            Assert.That(GameResult.Win, Is.EqualTo(result));
+        }
 
-    public void Game_PlayerWins_WithRockAgainstScissors()
-    {
-        var game = new Game();
-        var result = game.Play(Move.Rock, Move.Scissors);
-        Assert.That("You win!", Is.EqualTo(result));
-    }
-
-    [Test]
-
-    public void Game_PlayerLose_WithScissorsAgainstRock()
-    {
-        var game = new Game();
-        var result = game.Play(Move.Scissors, Move.Rock);
-        Assert.That("You loss!", Is.EqualTo(result));
-    }
-    
-    [Test]
-
-    public void Game_DrawWithSameMoves()
-    {
-        var game = new Game();
+        [Test]
+        public void DetermineWinner_PaperVsRock_Player1Wins()
+        {
+            var game = new Game(null, null, 1);
+            var result = game.DetermineWinner(Move.Paper, Move.Rock);
+            Assert.That(GameResult.Win, Is.EqualTo(result));
+        }
         
-        var result = game.Play(Move.Scissors, Move.Scissors);
-        Assert.That("Draw!", Is.EqualTo(result));
-        
-        result = game.Play(Move.Rock, Move.Rock);
-        Assert.That("Draw!", Is.EqualTo(result));
-        
-        result = game.Play(Move.Paper, Move.Paper);
-        Assert.That("Draw!", Is.EqualTo(result));
-    }
-    
-    [Test]
-    
-    public void Game_ComputerMoveIsRandom()
-    {
-        var game = new Game();
-        var result = game.GetPcMove();
-        Assert.That(result, Is.AnyOf(Move.Rock, Move.Paper, Move.Scissors));
-    }
+        [Test]
+        public void DetermineWinner_Draw()
+        {
+            var game = new Game(null, null, 1);
+            var result = game.DetermineWinner(Move.Rock, Move.Rock);
+            Assert.That(GameResult.Draw, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void DetermineWinner_Player2Wins()
+        {
+            var game = new Game(null, null, 1);
+            var result = game.DetermineWinner(Move.Rock, Move.Paper);
+            Assert.That(GameResult.Loss, Is.EqualTo(result));
+        }
+        [Test]
+        public void Play_BestOf3_Player1Wins()
+        {
+            var player1Mock = new Mock<IPlayer>();
+            player1Mock.SetupSequence(p => p.MakeMove())
+                .Returns(Move.Rock)
+                .Returns(Move.Paper)
+                .Returns(Move.Scissors);
+
+            var player2Mock = new Mock<IPlayer>();
+            player2Mock.SetupSequence(p => p.MakeMove())
+                .Returns(Move.Scissors)
+                .Returns(Move.Rock)
+                .Returns(Move.Paper);
+
+            var game = new Game(player1Mock.Object, player2Mock.Object, 3);
+            var result = game.Play();
+            Assert.That(GameResult.Win, Is.EqualTo(result));
+        }
+
 }
